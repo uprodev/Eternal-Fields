660878a4235a3ac712b0425260ebdf627eba3817
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-07-22 20:37:51 +0200

    Update join.php

--- theme-wp/eternal/templates/sections/join.php
+++ theme-wp/eternal/templates/sections/join.php
@@ -1,40 +1,44 @@
 <?php
 
-
+$image = get_sub_field('image');
+$title = get_sub_field('title');
+$text = get_sub_field('text');
+$form = get_sub_field('form');
 
 ?>
 
 <section class="join">
-    <div class="bg">
-        <img src="img/bg-6.jpg" alt="">
-    </div>
+    <?php if($image):?>
+        <div class="bg">
+            <img src="<?= $image['url'];?>" alt="<?= $image['alt'];?>">
+        </div>
+    <?php endif;?>
     <div class="content-width">
         <div class="form-wrap">
-            <h6 class="label">Join Our Community</h6>
-            <p>Subscribe to our newsletter for the latest on specials, new products, and exclusive events.</p>
-            <form action="#" class="form-default">
-                <div class="input-wrap">
-                    <label for="name"></label>
-                    <input type="text" name="name" id="name" placeholder="Name">
+            <?php if($title):?>
+                <h6 class="label"><?= $title;?></h6>
+            <?php endif;?>
+
+            <?php if($text):?>
+                <p><?= $text;?></p>
+            <?php endif;?>
+
+            <?php if($form):?>
+                <div class="form-default">
+                    <?= do_shortcode('[contact-form-7 id="'.$form.'"]');?>
                 </div>
-                <div class="input-wrap">
-                    <label for="email"></label>
-                    <input type="email" name="email" id="email" placeholder="Email">
-                </div>
-                <div class="input-wrap-submit">
-                    <button type="submit" class="btn-white">submit</button>
-                </div>
-            </form>
+            <?php endif;?>
+
         </div>
         <div class="title-wrap">
             <div class="img-wrap">
-                <img src="img/img-6-1.jpg" alt="">
-                <img src="img/img-6-2.jpg" alt="">
-                <img src="img/img-6-3.jpg" alt="">
+                <img src="<?= get_template_directory_uri();?>/img/img-6-1.jpg" alt="">
+                <img src="<?= get_template_directory_uri();?>/img/img-6-2.jpg" alt="">
+                <img src="<?= get_template_directory_uri();?>/img/img-6-3.jpg" alt="">
             </div>
             <h2>Stay Informed and Inspired</h2>
             <div class="btn-wrap">
-                <a href="#" class="btn-arrow">check our insta <img src="img/arrow-1.svg" alt=""></a>
+                <a href="#" class="btn-arrow">check our insta <img src="<?= get_template_directory_uri();?>/img/arrow-1.svg" alt=""></a>
             </div>
         </div>
     </div>
