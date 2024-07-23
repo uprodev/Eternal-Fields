40a155c5ce193b92e6d60728b9a8690d07e68cc5
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-07-23 11:25:13 +0200

    Update content-product.php

--- theme-wp/eternal/woocommerce/content-product.php
+++ theme-wp/eternal/woocommerce/content-product.php
@@ -23,14 +23,14 @@ global $product;
 if ( empty( $product ) || ! $product->is_visible() ) {
 	return;
 }
-$img = get_field('second_image');
+$img = get_field('card_image');
 ?>
 <div class="swiper-slide">
     <figure>
-        <a href="<?php the_permalink();?>"><img src="<?= $img?$img['url']:get_the_post_thumbnail_url();?>" alt=""></a>
+        <a href="<?php the_permalink();?>"><img src="<?= $img?$img['url']:get_the_post_thumbnail_url();?>" alt="<?= strip_tags(get_the_title());?>"></a>
     </figure>
     <div class="text">
-        <h6><a href="<?php the_permalink();?>"><?php the_title();?></a></h6>
+        <h6><a href="<?php the_permalink();?>"><?= strip_tags(get_the_title());?></a></h6>
         <p><?php the_excerpt();?></p>
         <a href="<?php the_permalink();?>"><img src="<?= get_template_directory_uri();?>/img/icon-2.svg" alt=""></a>
     </div>
