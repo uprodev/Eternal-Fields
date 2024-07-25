4609bcd3bd627f70d807fd643946da5c9b69c1a2
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-07-25 14:49:04 +0200

    locations

--- theme-wp/eternal/parts/map.php
+++ theme-wp/eternal/parts/map.php
@@ -1,6 +1,9 @@
 <?php
 
-
+$map = get_field('map_shortcode', 'options');
+$title = get_field('title_loc', 'options');
+$subtitle = get_field('subtitle_loc', 'options');
+$locations = get_field('locations', 'options');
 
 ?>
 
@@ -11,58 +14,51 @@
     <div class="content-width">
         <div class="title-wrap">
             <div class="title">
-                <h6 class="label">Location</h6>
-                <h2>Find a location near you</h2>
+                <?php if($subtitle):?>
+                    <h6 class="label"><?= $subtitle;?></h6>
+                <?php endif;?>
+                <?php if($title):?>
+                    <h2><?= $title;?></h2>
+                <?php endif;?>
             </div>
             <div class="form-wrap">
-                <form action="#" class="form-search">
-                    <label for="search"></label>
-                    <input type="search" id="search" name="search" placeholder="zip-code">
-                    <button type="submit" class="btn-white">Search</button>
-                </form>
+<!--                <form action="#" class="form-search">-->
+<!--                    <label for="search"></label>-->
+<!--                    <input type="search" id="search" name="search" placeholder="zip-code">-->
+<!--                    <button type="submit" class="btn-white">Search</button>-->
+<!--                </form>-->
             </div>
         </div>
 
-        <div class="content-map">
-            <img src="<?= get_template_directory_uri();?>/img/map.jpg" alt="">
-        </div>
-        <div class="map-info">
-            <div class="item">
-                <h6>DETROIT</h6>
-                <p>15440 E 8 Mile Rd,
-                    Detroit, MI 48205</p>
-                <p><a href="tel:+3132718201">(313) 271-8201</a></p>
-                <ul>
-                    <li>Hours</li>
-                    <li>M-F: 9am-6am</li>
-                    <li>Sat: 9am-6am</li>
-                    <li>Sun: Closed</li>
-                </ul>
+        <?php if($map):?>
+            <div class="content-map">
+                <?= do_shortcode(''.$map.'');?>
             </div>
-            <div class="item">
-                <h6>ferndale</h6>
-                <p>1461 E 8 Mile Rd,
-                    Ferndale, MI 48220</p>
-                <p><a href="tel:+2484390950">(248) 439-0950</a></p>
-                <ul>
-                    <li>Hours</li>
-                    <li>M-F: 9am-6am</li>
-                    <li>Sat: 9am-6am</li>
-                    <li>Sun: Closed</li>
-                </ul>
+        <?php endif;?>
+
+        <?php if($locations):?>
+            <div class="map-info">
+                <?php foreach($locations as $loc):?>
+                    <div class="item">
+
+                        <?= $loc['name']?'<h6>'.$loc['name'].'</h6>':'';?>
+
+                        <?= $loc['address']?'<p>'.$loc['address'].'</p>':'';?>
+
+                        <?= $loc['phone']?'<p><a href="tel:+'.phone_clear($loc['phone']).'">'.$loc['phone'].'</a></p>':'';?>
+
+                        <?php if($loc['horaries']):?>
+                            <ul>
+                                <?php foreach($loc['horaries'] as $hr):?>
+                                    <li><?= $hr['item'];?></li>
+                                <?php endforeach;?>
+                            </ul>
+                        <?php endif;?>
+
+                    </div>
+                <?php endforeach;?>
             </div>
-            <div class="item">
-                <h6>hamtramck</h6>
-                <p>2673 E Caniff St,
-                    Hamtramck, MI 48212</p>
-                <p><a href="tel:+3138555504">(313) 855-5504</a></p>
-                <ul>
-                    <li>Hours</li>
-                    <li>M-F: 9am-6am</li>
-                    <li>Sat: 9am-6am</li>
-                    <li>Sun: Closed</li>
-                </ul>
-            </div>
-        </div>
+        <?php endif;?>
+        
     </div>
 </section>
