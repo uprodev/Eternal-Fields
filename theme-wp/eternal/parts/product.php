1fa29582d01f4415c89c4c9c1a8d0daaf33ca340
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-07-29 13:27:19 +0200

    Update product.php

--- theme-wp/eternal/parts/product.php
+++ theme-wp/eternal/parts/product.php
@@ -3,9 +3,23 @@
 $img = get_field('card_image');
 $short = wpautop(get_the_excerpt());
 
+$sel = get_field('image_line');
+$class = '';
+if($sel == 'Symbol left - behind image'){
+    $class = 'after-left';
+}elseif($sel == 'Symbol right - behind image'){
+    $class = 'after-right';
+}elseif($sel == 'Symbol left - in front of image'){
+    $class = 'before-left';
+}elseif($sel == 'Symbol right - in front of image'){
+    $class = 'before-right';
+}else{
+    $class = '';
+}
+
 ?>
 <div class="swiper-slide">
-    <figure>
+    <figure class="<?= $class;?>">
         <a href="<?php the_permalink();?>"><img src="<?= $img?$img['url']:get_the_post_thumbnail_url();?>" alt="<?= strip_tags(get_the_title());?>"></a>
     </figure>
     <div class="text">
